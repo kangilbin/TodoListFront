f04d5d9c62dcaf3879038a81a175432c9bc4b501
kangilbin <kib3388@naver.com>
2021-12-30 23:00:33 +0900

    Todo View 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo';
+import AddTodo from './AddTodo';
+import React from 'react';
+import { Paper, List, Container } from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // (1) item -> items 배열로
+    this.state = { 
+      items: [
+        { id: "0", title: "Hello World 1", done: true }, 
+        { id: "1", title: "Hello World 2", done: false }, 
+      ],
+    };
+  }
+  render(){
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin: 16}}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} />
+          ))}
+        </List>
+      </Paper>
+    );
+
+    // (3) 생성된 컴포넌트 리턴
+    return (
+      <div className='App'>
+        <Container maxWidth='md'>
+          <AddTodo />
+          <div className='TodoList'>{todoItems}</div>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
