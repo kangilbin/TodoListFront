28476e05684ead9c013fde1f982f79edddf52f3b
kangilbin <kib3388@naver.com>
2022-01-19 23:44:02 +0900

    로컬 스토리지에 토큰 저장

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,10 +1,18 @@
 import { API_BASE_URL } from "../api-config";
+const ACCESS_TOKEN = "ACCESS_TOKEN";
 
 export function call(api, method, request) {
+    let headers = new Headers({
+        "Content-Type": "application/json",
+    });
+    // 로컬 스토리지에 ACCESS_TOKEN 가져오기
+    const accessToken = localStorage.getItem("ACCESS_TOKEN");
+        if(accessToken && accessToken !== null){
+            headers.append("Authorization", "Bearer " + accessToken);
+        }      
+
     let  options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
-        }),
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
@@ -34,7 +42,11 @@ export function call(api, method, request) {
 export function signin(userDTO) {
     return call("/auth/signin", "POST", userDTO)
         .then((response) => {
-            console.log("response : ", response);
-            alert("로그인 토큰: ", response.token);
+            if(response.token) {
+                // 로컬 스토리지에 토큰 저장
+                localStorage.setItem(ACCESS_TOKEN, response.token);
+                // token이 존재하지 않는 경우 Todo 화면으로 리디렉트
+                window.location.href = "/";
+            }
         });
 }
